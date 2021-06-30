48689b9ce2895ced295cf1017a52c271b84141e2
jhon jairo nuñez <jjnunezd9@gmail.com>
2021-06-30 18:28:45 -0500

    -

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,10 +7,14 @@ const router=express.Router();
 
 const Televisor = require('../models/televisor');         //importo modelos para poder utilizar mi esquema de objeto
 
-router.get('/',async(req,res)=>{ 
+router.get('/',(req,res)=>{           //en ruta raiz rendderizo index
+    res.render('index')
+})
+
+router.get('/inventario',async(req,res)=>{ 
     const televisores = await Televisor.find();            //task es manera de pedir datos a mi coleccion task, con task.find nos traemos datos desde base de datos y es asincrono
-    console.log(televisores);
-    res.render('index',{
+    //console.log(televisores);
+    res.render('inventario',{
         televisores   //task:task
     });                        //cuando hago peticion a ruta inicial de mi servidor renderizo una vista ejs de views (html), le paso a esa vista las tareas
 });
@@ -21,41 +25,29 @@ router.post('/add',async (req,res)=>{
     const televisor=new Televisor(req.body);              //cuando me contesta hago un nuevo objeto de modelo task con datos del body
     await televisor.save();                            //guardo en mi base de datos lo guardado en task
     //res.send('received');                         // mando un received si todo sale bien
-    res.redirect('/');                             //me redirecciona a ruta principal del servidor
-});
-
-
-router.get('/turn/:id',async (req,res)=>{
-    const{id}=req.params;                    //id que viene de objeto req.params, id q me envia navegador
-    const televisor=await Televisor.findById(id);         //busco id enviado por navegador
-    televisor.status=!televisor.status;
-    await televisor.save();
-    //console.log(task);
-    //res.send('received'); 
-    res.redirect('/'); 
+    res.redirect('/inventario');                             //me redirecciona a ruta principal del servidor
 });
 
 router.get('/edit/:id', async(req,res)=>{       //a servidor cuando me pidan edit con id cualquiera:
     const{id}=req.params;
-    const task=await Task.findById(id);
+    const televisor=await Televisor.findById(id);
     res.render('edit',{
-        task
+        televisor
     });
 });
 
-
-router.post('/edit/:id', async(req,res)=>{
+router.post('/edit/:id', async(req,res)=>{   //ACTUALIZAR
     const{id}=req.params;
-    await Task.updateOne({_id:id},req.body);
-    res.redirect('/'); 
+    await Televisor.updateOne({_id:id},req.body);
+    res.redirect('/inventario'); 
 });
 
 router.get('/delete/:id',async (req,res)=>{  //cuando el navegador me pida id es una variable o indice de mi elemento
    //console.log(req.params);
    const{id}=req.params;                    //id que viene de objeto req.params
    //res.send('received');
-    await Televisor.remove({_id:id});                 //de mi modelo task remuevo _id con el idde req.params
-    res.redirect('/');
+    await Televisor.remove({_id:id});                 //de mi modelo task remuevo _id con el id de req.params
+    res.redirect('/inventario');
 
 });            
 
